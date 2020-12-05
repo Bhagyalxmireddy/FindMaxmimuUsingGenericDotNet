4e67bcf032d143dcb2fac5b1e603a5e2c8f266c7
Bhagi <puppyreddy1997@gmail.com>
2020-12-05 19:40:08 +0530

    [Bhagyalaxmi]ADDED:printing the Welcome message

---
+++ FindMaximumValueUsingGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximumValueUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to FindingMaximum Value using generics");
+        }
+    }
+}
